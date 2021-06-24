6401c4c3fb251f6fe5c659f70ca42d50c046e728
sameep-sehgal <62800195+sameep-sehgal@users.noreply.github.com>
2021-06-24 11:28:17 +0530

    Updates

---
+++ precache-manifest.baa33f660299d05b48f788968d795615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7489431b9506948a3e721dd980309d91",
+    "url": "/DreamStream/index.html"
+  },
+  {
+    "revision": "a4226101b2cc64bf3ef8",
+    "url": "/DreamStream/static/js/2.ee394402.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/DreamStream/static/js/2.ee394402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be17c51e251170a6fd1",
+    "url": "/DreamStream/static/js/main.41ac8bd8.chunk.js"
+  },
+  {
+    "revision": "2558af27a6d4c2af9818",
+    "url": "/DreamStream/static/js/runtime-main.9161e674.js"
+  }
+]);
